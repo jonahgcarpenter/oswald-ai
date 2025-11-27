07b00c7a44261405395bcaa21bbd97721e4393c4
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-11-27 11:04:45 -0600

    listen on all interfaces

--- app/main.py
+++ app/main.py
@@ -105,5 +105,5 @@ app.include_router(agent_router, prefix="/agent")
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", host="127.0.0.1", port=8000, reload=True, log_config=LOGGING_CONFIG
+        "main:app", host="0.0.0.0", port=8000, reload=True, log_config=LOGGING_CONFIG
     )
